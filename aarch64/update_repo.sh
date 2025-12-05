ceb8fad1f9c92fb7e1f656e0f64abff791f16772
mschirrmeister <mschirrmeister@gmail.com>
2025-12-05 18:03:44 +0100

    update

--- aarch64/update_repo.sh
+++ aarch64/update_repo.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-rm archrepo*
+rm msc*
 
 echo "repo-add"
-repo-add -s -n -R archrepo.db.tar.zst *.pkg.tar.zst
+repo-add -s -n -R msc.db.tar.zst *.pkg.tar.zst
 
 sleep 1
 
-rm archrepo.db
-rm archrepo.db.sig
+rm msc.db
+rm msc.db.sig
 
-rm archrepo.files
-rm archrepo.files.sig
+rm msc.files
+rm msc.files.sig
 
-cp -p archrepo.db.tar.zst archrepo.db
-cp -p archrepo.db.tar.zst.sig archrepo.db.sig
+cp -p msc.db.tar.zst msc.db
+cp -p msc.db.tar.zst.sig msc.db.sig
 
-cp -p archrepo.files.tar.zst archrepo.files
-cp -p archrepo.files.tar.zst.sig archrepo.files.sig
+cp -p msc.files.tar.zst msc.files
+cp -p msc.files.tar.zst.sig msc.files.sig
 
 echo "####################################"
 echo "Repo Updated!!"
